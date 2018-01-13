30dbcfddd318b2ecce1139e203972416a2edb86d
me <me@mes-iMac.local>
2018-01-12 16:36:52 -0800

    Initial Commit

---
+++ BlueStream-PoC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueStream-PoC
+//
+//  Created by me on 1/12/18.
+//  Copyright © 2018 Arizona State University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
